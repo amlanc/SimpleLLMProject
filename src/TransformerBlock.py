8f3fb6d28add2c0e4d26b859527eece47a8926a0
Amlan Chatterjee <amlanc@gmail.com>
2025-01-17 12:13:00 -0800

    Completed building of Layernorm, GELU, Feedforward network and learned about Shortcut connections as well.

---
+++ src/TransformerBlock.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+from MultiHeadAttention import MultiHeadAttention
+from SimpleFeedForward import SimpleFeedForward
+
+
+class TransformerBlock(nn.Module):
+
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = SimpleFeedForward(cfg)
+        self.norm1 = nn.LayerNorm(cfg["emb_dim"])
+        self.norm2 = nn.LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x += shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x += shortcut
+
+        return x
+
+
