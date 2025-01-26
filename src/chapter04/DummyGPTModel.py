7db2dcc28f3809b7e2b99626f9fbad5db459f55d
Amlan Chatterjee <amlanc@gmail.com>
2025-01-26 11:31:31 -0800

    Added device mps awareness

--- src/chapter04/DummyGPTModel.py
+++ src/chapter04/DummyGPTModel.py
@@ -1,31 +1,38 @@
 import torch
 import torch.nn as nn
+import os
 
 
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
-        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        if torch.backends.mps.is_available() and os.name == "posix":
+            device = torch.device('mps:0')
+        else:
+            device = torch.device('cpu')
+        self.device = device
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"]).to(self.device)
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"]).to(self.device)
+        self.drop_emb = nn.Dropout(cfg["drop_rate"]).to(self.device)
 
         # Unpack the DummyTransformerBlock objects initialized with config in the config array of n_layers
         # and create sequential layers of them
         self.trf_blocks = nn.Sequential(*[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])])
 
-        self.final_norm = DummyLayerNorm(cfg["emb_dim"])
+        self.final_norm = DummyLayerNorm(cfg["emb_dim"]).to(self.device)
         self.out_head = nn.Linear(
             cfg["emb_dim"], cfg["vocab_size"], bias=False
-        )
+        ).to(self.device)
 
     def forward(self, in_idx):
         batch_size, seq_len = in_idx.shape
-        tok_embeds = self.tok_emb(in_idx)
-        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        tok_embeds = self.tok_emb(in_idx).to(self.device)
+        pos_embeds = self.pos_emb(torch.arange(seq_len).to(self.device)).to(self.device)
         x = tok_embeds + pos_embeds
         x = self.drop_emb(x)
         x = self.trf_blocks(x)
         x = self.final_norm(x)
+        x.to(self.device)
         logits = self.out_head(x)
         return logits
 
--- src/chapter04/GPTModel.py
+++ src/chapter04/GPTModel.py
@@ -1,42 +1,58 @@
 import os
 import torch
 from torch import nn
-from LayerNorm import LayerNorm
-from TransformerBlock import TransformerBlock
+from src.chapter04.LayerNorm import LayerNorm
+from src.chapter04.TransformerBlock import TransformerBlock
 
 
 class GPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
-        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
-        self.drop_emb = nn.Dropout(cfg["drop_rate"])
         if torch.backends.mps.is_available() and os.name == "posix":
             device = torch.device('mps:0')
         else:
             device = torch.device('cpu')
         self.device = device
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"]).to(device=self.device)
+        # print(f"__init__(): tok_emb created")
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"]).to(device=self.device)
+        # print(f"__init__(): pos_emb created")
+        self.drop_emb = nn.Dropout(cfg["drop_rate"]).to(device=self.device)
+        # print(f"__init__(): dropout layer created")
         self.transformer_blocks = nn.Sequential(
             *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])]
-        )
+        ).to(device=self.device)
+        # print(f"__init__(): transformers created")
         self.final_norm = LayerNorm(cfg["emb_dim"])
+        # print(f"__init__(): LayerNorm created")
         # Exact same dim as it came in
         self.out_head = nn.Linear(
             cfg["emb_dim"], cfg["vocab_size"], bias=False
         )
+        # print(f"__init__(): Linear out projection layer created\n")
 
     def forward(self, in_idx):
         # if torch.backends.mps.is_available():
-        #     device = torch.device('mps')
-        print("forward(): Input Shape: ", in_idx.shape)
+        #     self.device = torch.device('mps')
         batch_size, seq_len = in_idx.shape
-        tok_embeds = self.tok_emb(in_idx)
-        print(f"forward(): device = {self.device}")
-        pos_embeds = self.pos_emb(torch.arange(seq_len))
+        print(f"forward(): batch_size: {batch_size}, seq_len: {seq_len} and Input Shape: {in_idx.shape}")
+
+        in_idx.to(self.device)
+        tok_embeds = self.tok_emb(in_idx).to(self.device)
+        print("forward(): created tok_embeds")
+
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len).to(self.device)
+        ).to(self.device)
+        print("forward(): created pos_embeds")
+
         # x = torch.cat((tok_embeds, pos_embeds), dim=-1)
         x = tok_embeds + pos_embeds
+
+        print(f"forward(): x.shape: {x.shape}")
         x = self.drop_emb(x)
         x = self.transformer_blocks(x)
         x = self.final_norm(x)
         logits = self.out_head(x)
+        print(f"forward(): logits.shape: {logits.shape}")
         return logits
